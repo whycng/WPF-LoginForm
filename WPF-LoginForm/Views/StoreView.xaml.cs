7571afbb7ca24e7ed46549a6fd9056d5da8669e5
whycng <61927727+whycng@users.noreply.github.com>
2023-05-15 16:41:22 +0800

    准备丰富一些页面，修改商店页面，修复bug

---
+++ WPF-LoginForm/Views/StoreView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_LoginForm.Views
+{
+    /// <summary>
+    /// StoreView.xaml 的交互逻辑
+    /// </summary>
+    public partial class StoreView : Window
+    {
+        public StoreView()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
