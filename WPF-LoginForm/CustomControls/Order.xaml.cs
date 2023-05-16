9a8fcd7fa2eac73cf339c4976d9d2a8c1c637d94
whycng <61927727+whycng@users.noreply.github.com>
2023-05-16 19:13:15 +0800

    Store View放在设置里面，测试一下，图没有正常显示

---
+++ WPF-LoginForm/CustomControls/Order.xaml.cs
@@ -0,0 +1,49 @@
+﻿ 
+using System.Windows;
+using System.Windows.Controls; 
+
+namespace WPF_LoginForm.CustomControls
+{
+    /// <summary>
+    /// Order.xaml 的交互逻辑
+    /// </summary>
+    public partial class Order : UserControl
+    {
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty);} 
+            set { SetValue(TitleProperty, value);}   
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(Order));
+
+        public string Desc
+        {
+            get { return (string)GetValue(DescProperty); }
+            set { SetValue(DescProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescProperty =
+            DependencyProperty.Register("Desc", typeof(string), typeof(Order));
+
+
+
+        public FontAwesome.Sharp.IconChar Icon
+        {
+            get { return (FontAwesome.Sharp.IconChar)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(FontAwesome.Sharp.IconChar), typeof(Order));
+
+
+
+    }
+}
